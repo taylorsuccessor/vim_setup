cca57887b5bbdf11edb20725a9fde186f56e03bb
hashim_mac_m3_air <taylorsuccessor@gmail.com>
2024-09-18 22:37:26 +0300

    add required permission to edit nginx

--- nginx/nginx_logs_enable_disable.sh
+++ nginx/nginx_logs_enable_disable.sh
@@ -9,7 +9,7 @@ ENABLED_LINK="${NGINX_CONF_DIR}/nginx_full_logs_enabled.conf"
 # Enable full logs
 enable_logs() {
     if [ -f "$FULL_LOGS_CONF_FILE_LOCATION" ] && [ ! -L "$ENABLED_LINK" ]; then
-        ln -s "$FULL_LOGS_CONF_FILE_LOCATION" "$ENABLED_LINK"
+        sudo ln -s "$FULL_LOGS_CONF_FILE_LOCATION" "$ENABLED_LINK"
 
         sudo systemctl reload nginx
         echo "Full logging enabled."
@@ -21,7 +21,7 @@ enable_logs() {
 # Disable full logs
 disable_logs() {
     if [ -L "$ENABLED_LINK" ]; then
-        rm "$ENABLED_LINK"
+        sudo rm "$ENABLED_LINK"
 
         sudo systemctl reload nginx
         echo "Full logging disabled."
@@ -40,7 +40,7 @@ case "$1" in
         ;;
     *)
         echo "Usage: $0 {enable|disable}"
-        exit 1
+        #exit 1
         ;;
 esac
 
